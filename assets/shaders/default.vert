c57c16d39203b683b634dd13f0aa8eee4200994a
NkosiDaniel <xxxenouniverse@gmail.com>
2025-11-18 23:19:55 -0500

    - Manipulated the vertices with the MVP matrix - Managed to rotate our object through time - Enabled depth testing so the appropiate pixels are drawn on top of each other

--- assets/shaders/default.vert
+++ assets/shaders/default.vert
@@ -6,11 +6,14 @@ layout(location = 2) in vec2 aTex;
 out vec2 texCoord; // Output the coordinates of our texture
 out vec3 color; // This will output this variable to the fragment shader
 
-uniform float scale; // Is inputted from the CPU to all parallel threads on the GPU
+ // Uniforms are inputted from the CPU to all parallel threads on the GPU
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 proj;
 
 void main()
 {
-    gl_Position = vec4(aPos.x + aPos.x * scale, aPos.y + aPos.y * scale, aPos.z + aPos.z * scale, 1.0);
+    gl_Position = proj * view * model * vec4(aPos, 1.0);
     color = aColor;
     texCoord = aTex;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,40 +3,49 @@
 #include <GLFW/glfw3.h>
 #include <cmath>
 
-
 #include "../headers/shaderClass.h"
 #include "../headers/EBO.h"
 #include "../headers/VBO.h"
 #include "../headers/VAO.h"
 #include "../headers/texture.h"
-#include "../include/stb/stb_image.h"
 
+#include "../include/stb/stb_image.h"
+#include "../include/glm/glm.hpp"
+#include "../include/glm/gtc/matrix_transform.hpp"
+#include "../include/glm/gtc/type_ptr.hpp"
+
+const unsigned int width = 800;
+const unsigned int height = 800;
 
 int main(int, char**){
    // Storing our values of each vertex in our coordinate space
    GLfloat vertices[] = 
    {    // POSITIONS            // COLORS      // TEXT COORD
-     -0.5f, -0.5f, 0.0f,    0.8f, 0.3f, 0.02f,  0.0f, 0.0f, // Bottom left corner of square
-     -0.5,   0.5f, 0.0f,    0.8f, 0.3f, 0.02f,  0.0f, 1.0f, // Top left corner of square
-      0.5f,  0.5f, 0.0f,    1.0f, 6.0f, 0.32f,  1.0f, 1.0f, // Top right corner of square
-      0.5f, -0.5,  0.00,    0.9f, 0.45, 0.17f,  1.0f, 0.0f // Bottom right corner of square
+     -0.5f,  0.0f, 0.5f,    0.8f, 0.3f, 0.02f,  0.0f, 0.0f, 
+     -0.5,   0.0f,-0.5f,    0.8f, 0.3f, 0.02f,  5.0f, 1.0f, 
+      0.5f,  0.0f,-0.5f,    1.0f, 6.0f, 0.32f,  0.0f, 0.0f, 
+      0.5f, -0.0,  0.6f,    0.9f, 0.45, 0.17f,  5.0f, 0.0f,
+      0.0f,  0.8f, 0.0f,    0.92f,0.86f,0.76f,   2.5f, 5.0f
    };
 
    // Index Buffer
    GLuint indices[] = 
    { 
-     0, 2, 1, // Upper triangle
-     0, 3, 2 // Lower triangle
+     0, 1, 2,
+     0, 2, 3,
+     0, 1, 4,
+     1, 2, 4,
+     2, 3, 4,
+     3, 0, 4
    };
-
-
+   
    GLFWwindow* window;
 
    if(!glfwInit()) {
         return -1;
    }
    // Create the window that appears on the screen
-   window = glfwCreateWindow(800, 800, "Window", NULL, NULL);
+   window = glfwCreateWindow(width, height, "Window", NULL, NULL);
 
    // Tells GLFW to add the window to the current context
    // A context being an object that encapsulates the OpenGL state machine
@@ -72,18 +81,48 @@ int main(int, char**){
    Texture myTex("../brick_12-512x512.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
    myTex.texUnit(shaderProgram, "tex0", 0);
 
+   float rotation = 0.0f;
+   double prevTime = glfwGetTime();
+
+   glEnable(GL_DEPTH_TEST);
+
    while(!glfwWindowShouldClose(window)) {
         // Specify color of background
         glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
         // Clean the back buffer and assign the new color to it
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         // Tell OpenGL which shader program we want to use
         shaderProgram.Activate();
+
+        double crntTime = glfwGetTime();
+        if(crntTime - prevTime >= 1 / 60.0) 
+        {
+          rotation += 0.5f;
+          prevTime = crntTime;
+        }
+
+        glm::mat4 model = glm::mat4(1.0f);
+        glm::mat4 view = glm::mat4(1.0f);
+        glm::mat4 proj = glm::mat4(1.0f);
+
+        model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
+        view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+        proj = glm::perspective(glm::radians(45.0f), (float)(width / height), 0.1f, 100.0f);
+
+        int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
+
+
         myTex.Bind();
         // Bind the VAO so OpenGL knows to use it
         VAO1.Bind();
         // Draw primitives, number of indices, datatype of indices, index of indices
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(int), GL_UNSIGNED_INT, 0);
         // Swap back buffer with front buffer
         glfwSwapBuffers(window);
         // Makes sure our window is responsive (such as resizing it and moving it)
